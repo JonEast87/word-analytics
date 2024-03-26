11d39589b579ffde50b80ed60ac3f91a238b11d3
jdeastin <j.d.east@icloud.com>
2024-03-25 18:15:34 -0700

    needed to lift state in order to rework the text and settext to allow stat tracking

--- src/Container.jsx
+++ src/Container.jsx
@@ -1,11 +1,15 @@
 import Textarea from "./Textarea";
 import Stats from "./Stats";
+import { useState } from "react";
 
 export default function Container() {
+  const [text, setText] = useState("");
+  const numberOfCharacters = text.length;
+
   return (
     <main className="container">
-      <Textarea />
-      <Stats />
+      <Textarea text={text} setText={setText} />
+      <Stats numberOfCharacters={numberOfCharacters} />
     </main>
   );
 }
