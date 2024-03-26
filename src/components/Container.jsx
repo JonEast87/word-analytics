54e44dc8d631f622fd399d488685935d1baf2cc3
jdeastin <j.d.east@icloud.com>
2024-03-25 18:44:02 -0700

    updated file structure to tighten everything up

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,5 +1,6 @@
 import Textarea from "./Textarea";
 import Stats from "./Stats";
+import { FACEBOOK_MAX_CHARACTERS, INSTAGRAM_MAX_CHARACTERS } from "../lib/constants";
 import { useState } from "react";
 
 export default function Container() {
@@ -8,8 +9,8 @@ export default function Container() {
   const stats = {
     numberOfWords: text.split(/\s/).filter((word) => word !== "").length,
     numberOfCharacters: text.length,
-    instagramCharactersLeft: 280 - text.length,
-    facebookCharactersLeft: 2200 - text.length,
+    instagramCharactersLeft: INSTAGRAM_MAX_CHARACTERS - text.length,
+    facebookCharactersLeft: FACEBOOK_MAX_CHARACTERS - text.length,
   };
 
   return (
